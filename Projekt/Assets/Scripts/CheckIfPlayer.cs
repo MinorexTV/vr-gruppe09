dce3febbaf745deb53a0f19b478c115919fbe1bc
Nouri <nouribn16@gmail.com>
2022-11-04 17:37:51 +0100

    added left hand func + fov visual

---
+++ Projekt/Assets/Scripts/CheckIfPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckIfPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private bool isPlayer = false;
+    // Start is called before the first frame update
+
+    public void HitPlayer()
+    {
+
+    }
+    
+    public void HitNPC()
+    {
+        
+    }
+}
+    
