d0c405770e50980342a5b3e700993bce9edd4531
Nouri <nouribn16@gmail.com>
2022-12-13 14:35:47 +0100

    added visual guesses to vr

--- Projekt/Assets/Scripts/GuessesController.cs
+++ Projekt/Assets/Scripts/GuessesController.cs
@@ -1,11 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 public class GuessesController : MonoBehaviour
-{
-    TMP_Text textComp;
+{   [SerializeField]
+    private GameObject guess1;
+    [SerializeField]
+    private GameObject guess2;
+    [SerializeField]
+    private GameObject guess3;
+
+    private void Start()
+    {
+        guess1.SetActive(true);
+        guess2.SetActive(true);
+        guess3.SetActive(true);
+        GameManager.instance.guessesChanged.AddListener(UpdateGuesses);
+    }
+
+    void UpdateGuesses(int value)
+    {
+        if (value == 3)
+        {
+            guess1.SetActive(true);
+            guess2.SetActive(true);
+            guess3.SetActive(true);
+        }
+        else if (value == 2)
+        {
+            guess1.SetActive(false);
+            guess2.SetActive(true);
+            guess3.SetActive(true);
+        }
+        else if (value == 1)
+        {
+            guess1.SetActive(false);
+            guess2.SetActive(false);
+            guess3.SetActive(true);
+        }
+    }
+    /*TMP_Text textComp;
     void Start()
     {
         textComp = GetComponent<TMP_Text>();
@@ -17,5 +53,5 @@ public class GuessesController : MonoBehaviour
     public void UpdateText(int value)
     {
         textComp.text = "Versuche: " + value;
-    }
+    }*/
 }
